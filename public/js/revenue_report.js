c96f5d5d055f01ba0d59f35b4d1bbce31141e000
quanggvn <giapvanngocquang@gmail.com>
2024-04-08 23:57:57 +0700

    Calucate profit

--- public/js/revenue_report.js
+++ public/js/revenue_report.js
@@ -25,8 +25,9 @@ $(document).ready(function (string) {
                 let failed;
                 let numberOfTotal;
                 let total;
+                let profit;
                 let numberOfFailed;
-                total = failed = numberOfTotal = numberOfFailed = 0;
+                total = failed = numberOfTotal = numberOfFailed = profit = 0;
                 // date, failed, revenue, total order, profit
                 if (STATUS_FAILED === order.status_id) {
                     failed = order.total;
@@ -34,12 +35,14 @@ $(document).ready(function (string) {
                 } else {
                     total = order.total;
                     numberOfTotal = 1;
+                    profit = order.total + order.ship_by_customer - order.ship_by_shop - order.cost;
                 }
 
                 if (isset) {
                     statisticalMonth[order.order_date].revenue += total;
                     statisticalMonth[order.order_date].numberOfTotal += numberOfTotal;
                     statisticalMonth[order.order_date].failed += failed;
+                    statisticalMonth[order.order_date].profit += profit;
                     statisticalMonth[order.order_date].numberOfFailed += numberOfFailed;
                 } else {
                     statisticalMonth[order.order_date] = {
@@ -47,6 +50,7 @@ $(document).ready(function (string) {
                         numberOfTotal: numberOfTotal,
                         failed: failed,
                         numberOfFailed: numberOfFailed,
+                        profit: profit,
                         order_date: order.order_date,
                     }
                 }
@@ -62,6 +66,8 @@ $(document).ready(function (string) {
                 '<th style="" class="failed">Failed</th>\n' +
                 '<th style="" class="number-of-revenue">Number Of Revenue</th>\n' +
                 '<th style="" class="revenue">Revenue</th>\n' +
+                '<th style="" class="profit">Profit</th>\n' +
+                '<th style="" class="profit-percent">% Profit</th>\n' +
                 '</tr>\n' +
                 '</thead>\n' +
                 '<tbody>\n';
@@ -82,6 +88,7 @@ $(document).ready(function (string) {
             let sumFailed = 0;
             let sumNumberOfTotal = 0;
             let sumRevenue = 0;
+            let sumProfit = 0;
             while (moment(index, 'DD/MM/YYYY') <= moment(to, 'DD/MM/YYYY')) {
                 html += '<tr class="active">';
                 html += '<td>' + index + '</td>';
@@ -93,12 +100,17 @@ $(document).ready(function (string) {
                     sumFailed += data.failed;
                     sumNumberOfTotal += data.numberOfTotal;
                     sumRevenue += data.revenue;
+                    sumProfit += data.profit;
 
                     html += '<td>' + data.numberOfFailed + '</td>';
                     html += '<td>' + data.failed + '</td>';
                     html += '<td>' + data.numberOfTotal + '</td>';
                     html += '<td>' + getPriceFormat(data.revenue) + '</td>';
+                    html += '<td>' + getPriceFormat(data.profit) + '</td>';
+                    html += '<td>' + (100 * (data.profit / data.revenue)).toFixed(2) + ' %</td>';
                 } else {
+                    html += '<td></td>';
+                    html += '<td></td>';
                     html += '<td></td>';
                     html += '<td></td>';
                     html += '<td></td>';
@@ -116,6 +128,8 @@ $(document).ready(function (string) {
             html += '<td>' + sumFailed + '</td>';
             html += '<td>' + sumNumberOfTotal + '</td>';
             html += '<td>' + getPriceFormat(sumRevenue) + '</td>';
+            html += '<td>' + getPriceFormat(sumProfit) + '</td>';
+            html += '<td>' + (100 * (sumProfit / sumRevenue)).toFixed(2) + ' %</td>';
             html += '</tr>';
 
             html += '</tbody>\n' +
@@ -126,6 +140,8 @@ $(document).ready(function (string) {
                 '<th style="" class="failed">Failed</th>\n' +
                 '<th style="" class="number-of-revenue">Number Of Revenue</th>\n' +
                 '<th style="" class="revenue">Revenue</th>\n' +
+                '<th style="" class="revenue">Profit</th>\n' +
+                '<th style="" class="revenue">% Profit</th>\n' +
                 '</tr>\n' +
                 '</thead>\n' +
                 '</table>';
